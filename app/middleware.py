f785454260fe91e7eda52ad671fb39a75c41907c
DemaPy <benderdema@gmail.com>
2025-08-28 17:25:53 +0200

    fix: use custom header with x-*

--- app/middleware.py
+++ app/middleware.py
@@ -5,6 +5,6 @@ from fastapi import Header, HTTPException
 API_TOKEN = os.getenv("API_TOKEN")
 
 
-def verify_token(token: Optional[str] = Header(default=None)):
-    if API_TOKEN and token != API_TOKEN:
+def verify_token(x_token: Optional[str] = Header(default=None)):
+    if API_TOKEN and x_token != API_TOKEN:
         raise HTTPException(status_code=401, detail="Invalid API token")
